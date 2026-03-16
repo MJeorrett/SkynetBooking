f1947638e6202601a7ce626461953ba0775946e7
MJeorrett <matthewjeorrett@gmail.com>
2026-03-16 23:53:00 +0000

    remove speakers notes

---
+++ SkynetBooking.WebApi.E2eTests/Bookings/03-TidyUp-Before.cs
@@ -0,0 +1,65 @@
+using System.Net;
+using System.Net.Http.Json;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Shouldly;
+using SkynetBooking.Application.Bookings.Commands;
+using SkynetBooking.Core;
+using SkynetBooking.Infrastructure.Db;
+using SkynetBooking.WebApi.E2eTests.Shared.WebApplicationFactory;
+
+namespace SkynetBooking.WebApi.E2eTests.Bookings;
+
+public class Example3_Before
+{
+    [Fact]
+    public async Task Should_Return400_When_EndIsBeforeStart()
+    {
+        // Boilder plate
+        var factory = new CustomWebApplicationFactory();
+
+        // Boilder plate
+        using (var scope = factory.Services.CreateScope())
+        {
+            var context = scope.ServiceProvider.GetRequiredService<SkynetDbContext>();
+            context.Database.EnsureDeleted();
+            context.Database.Migrate();
+        }
+
+        // Boilder plate
+        await factory.ResetState();
+
+        // Boilder plate
+        var client = factory.CreateClient();
+
+        int aiCustomerId;
+        int humanResourceId;
+
+        using (var scope = factory.Services.CreateScope())
+        {
+            var context = scope.ServiceProvider.GetRequiredService<SkynetDbContext>();
+            var aiCustomer = new AiCustomerEntity { FullName = "E2E Test Customer" };
+            var humanResource = new HumanResourceEntity();
+            context.AiCustomers.Add(aiCustomer);
+            context.HumanResources.Add(humanResource);
+            await context.SaveChangesAsync();
+
+            aiCustomerId = aiCustomer.Id;
+            humanResourceId = humanResource.Id;
+        }
+
+        var start = new DateTime(2025, 3, 16, 12, 0, 0);
+
+        var request = new CreateBookingCommand
+        {
+            AiCustomerId = aiCustomerId,
+            HumanResourceId = humanResourceId,
+            Start = start,
+            End = start.AddHours(-1)
+        };
+
+        var response = await client.PostAsJsonAsync("api/bookings", request);
+
+        response.StatusCode.ShouldBe(HttpStatusCode.BadRequest);
+    }
+}
